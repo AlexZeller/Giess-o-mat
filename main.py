526d3ba2a67014adf00f2264408875d456ad8fbf
tmoelter <tina.moelter@live.de>
2020-05-04 21:49:24 +0200

    Comments and new function ventilation_set()

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ RH = dht22.get_humidity()
 # Set sensor thresholds
 soilH_min = 30 # soil humidity
 Ta_max = 30 # maximum air temperature
-RH_max = 80 # maximum air humidity
-lux_threshold = 4000 # minimum lux value
+RH_max = 70 # maximum air humidity
+lux_threshold = 5000 # minimum lux value
 
 # Set start and end time of lightning
 start_hour = 7
@@ -23,14 +23,17 @@ start_time = int(start_hour)*60 + int(start_minute)
 end_time = int(end_hour)*60 + int(end_minute)
 
 ############################################################################
-def light_time(start, end):
+def light_time(start, end, GPIO=17):
     """
     Depending of the current time the light is switched on (start < current
     time < end )or off (start > current time, stop < current time).
+    Arguments:
+        start (int): Start time in minutes when lightning shall start.
+        end (int): End time in minutes when lightning shall stop.
+        GPIO (int): GPIO pin used to switch Relais for light control.
     """
 
-    # Light GPIO = 17
-    light = Relais.Relais(17)
+    light = Relais.Relais(GPIO)
     current_time =  datetime.datetime.now().hour*60 + datetime.datetime.now().minute
     if start <= current_time and end >= current_time:
         light.on()
@@ -38,15 +41,22 @@ def light_time(start, end):
         light.off()
 
 
-def light_time_light(start, end, lux_threshold):
+def light_time_light(start, end, lux_threshold, GPIO=17, channel=1):
     """
     Depending of the current time and the current lux_value the light is
     switched on (start < current time < end AND lux_value < lux threshold) or
     off (start > current time, stop < current time OR lux_value > lux_threshold).
+
+    Arguments:
+        start (int): Start time in minutes when lightning shall start.
+        end (int): End time in minutes when lightning shall stop.
+        lux_threshold (int): Minimum value excepted before additional light source is needed.
+        GPIO (int): GPIO pin used to switch Relais for light control.
+        channel (int): Channel of ADC.
     """
 
-    light = Relais.Relais(17)
-    photoresistor = Photoresistor.Photoresistor(1)
+    light = Relais.Relais(GPIO)
+    photoresistor = Photoresistor.Photoresistor(channel)
     # Get current lux value
     lux_reading = photoresistor.get_lux()
     
@@ -56,23 +66,46 @@ def light_time_light(start, end, lux_threshold):
     else:
         light.off()
 
-# To DO: Write the function! Not usable yet.
-def ventilation_set(first, second, duration):
+
+def ventilation_set(times, GPIO=18):
     """
-    Ventilation is executed twice a day specified with first and second.
+    Ventilation is executed at specific times.
+    
+    Arguments:
+        times (array): Each time in times is a 2D Array 
+            time (array): [start_hour (int), start_min (int)]
+        GPIO (int): GPIO pin used to switch Relais for ventilation control.
     """
 
-    ventilation = Relais.Relais(18)
+    ventilation = Relais.Relais(GPIO)
+    # Get current time
+    current_time =  datetime.datetime.now().hour*60 + datetime.datetime.now().minute
+    # Compare time with current time 
+    for time in times:
+        tmp = time[0]*60 + time[1]
+        time_delta = tmp - current_time
+        if time_delta <= 0 and time_delta > -10:
+            ventilation.on()
+            break
+        else:
+            ventilation.off()
 
 
-def ventilation_auto(Ta, RH, Ta_max, RH_max):
+
+def ventilation_auto(Ta, RH, Ta_max, RH_max, GPIO=18):
     """
     Compares Ta with Ta_max and RH with RH_max. If statement is true/false
     ventilation starts/stops.
+    
+    Arguments:
+        Ta (float): Actual air temperature measured with DHT22.
+        RH (float): Actual humidity measured with DHT22.
+        Ta_max (int): Maximum air temperature accepted before ventialtion starts.
+        RH_max (int): Maximum humidity (in percent) accepted before ventilation starts.
+        GPIO (int): GPIO pin used to switch Relais for ventilation control.
     """
 
-    # Ventilation GPIO = 18
-    ventilation = Relais.Relais(18)
+    ventilation = Relais.Relais(GPIO)
     if Ta >= Ta_max:
         ventilation.on()
     elif RH >= RH_max:
@@ -82,15 +115,21 @@ def ventilation_auto(Ta, RH, Ta_max, RH_max):
 
 
 # To DO: Wie lange wird bewaessert?
-def watering_auto(soilH, soilH_min):
+def irrigation_auto(soilH, soilH_min, GPIO1, GPIO2):
     """
-    Compares soilH with soilH_min. If statement is true watering starts.
+    Compares soilH with soilH_min. If statement is true irrigation starts.
+    
+    Arguments:
+        soilH: Actual soil moisture measured with soil moisture sensor.
+        soilH_min: Minimum soil moisture accepted before irrigation starts.
+        GPIO1: GPIO pin used to switch Relais for pump control.
+        GPIO2: GPIO pin used to switch Relais for valve control.
     """
 
     # Pump GPIO = 
-    pump = Relais.Relais()
+    pump = Relais.Relais(GPIO1)
     # Valve GPIO = 
-    valve = Relais.Relais()
+    valve = Relais.Relais(GPIO2)
     if soilH <= soilH_min:
         valve.on()
         pump.on()
@@ -103,4 +142,5 @@ if __name__ == '__main__':
     #light_time(start_time, end_time)
     light_time_light(start_time, end_time, lux_threshold)
     ventilation_auto(Ta, RH, Ta_max, RH_max)
+    #ventilation_set([[21, 45], [22,0]], GPIO=18)
     
